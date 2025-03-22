850de650ca9b8fed5fa21e4b4ac794511fa5e696
Yukiice <13336340366@163.com>
2025-03-22 23:10:10 +0800

    v3.8

--- acwing/【3】搜索与图论/有边数限制的最短路.cpp
+++ acwing/【3】搜索与图论/有边数限制的最短路.cpp
@@ -4,11 +4,46 @@
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pp;
+// 存在负边，存在负环，限制边数为k，使用floyd
 
-const int N = 100010;
+const int N = 510, M = 10010;
+int n, m, k;
+vector<vector<pp>> g(N);
+int dis[N];
+int temp[N];
+
+// struct edge
+// {
+//     int a, b, w;
+// }edge[M];
+
+void ford(){
+    dis[1] = 0;
+    for(int i = 1;i <= k;i++){
+        // 备份dis数组，每次迭代的时候，只用上一次迭代结束后更新的dis，而不是迭代过程中的dis
+        memcpy(temp, dis, sizeof dis);
+        for(int j = 1;j <= n;j++){
+            for(pp t : g[j]){
+                int y = t.first;
+                int w = t.second;
+                dis[y] = min(dis[y], temp[j] + w);
+            }
+        }
+    }
+
+    if(dis[n] > 0x3f3f3f3f / 2) cout << "impossible";
+    else cout << dis[n];
+}
 
 void solve(){
-    
+    cin >> n >> m >> k;
+    memset(dis, 0x3f, sizeof dis);
+    for(int i = 1;i <= m;i++){
+        int x, y, z;
+        cin >> x >> y >> z;
+        g[x].push_back({y, z});
+    }
+    ford();
 }
 
 int main(){
