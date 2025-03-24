3732e90045021db9113dfbed299c00f8a94f15ba
Yukiice <13336340366@163.com>
2025-03-25 00:08:11 +0800

    v3.12

--- acwing/【3】搜索与图论/二分图的最大匹配.cpp
+++ acwing/【3】搜索与图论/二分图的最大匹配.cpp
@@ -5,10 +5,41 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pp;
 
-const int N = 100010;
+const int N = 1010, M = 100010;
+vector<vector<int>> g(N);
+int match[N];
+bool st[N];
+int n1, n2, m;
+
+bool find(int x){
+    for(int j : g[x]){
+        if(!st[j]){
+            st[j] = true;
+            if(match[j] == 0 || find(match[j])){
+                match[j] = x;
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
 
 void solve(){
+    cin >> n1 >> n2 >> m;
+    for(int i = 1;i <= m;i++){
+        int a, b;
+        cin >> a >> b;
+        g[a].push_back(b);
+    }
     
+    int ans = 0;
+    for(int i = 1;i <= n1;i++){
+        memset(st, false, sizeof st);
+        if(find(i)) ans++;
+    }
+
+    cout << ans << endl;
 }
 
 int main(){
