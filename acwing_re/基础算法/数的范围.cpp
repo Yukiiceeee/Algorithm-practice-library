201cf970a71de084ecf8ce756967ea8fdd8c2121
Yukiice <13336340366@163.com>
2025-03-29 23:00:58 +0800

    rev1.2

--- acwing_re/基础算法/数的范围.cpp
+++ acwing_re/基础算法/数的范围.cpp
@@ -7,8 +7,46 @@ typedef pair<int, int> pp;
 
 const int N = 100010;
 
+int n, q;
+vector<int> vtr(N);
+
+void binary_search_left(vector<int> &vtr, int k){
+    int l = 0, r = n-1;
+    int mid = 0;
+    while(l < r){
+        mid = (l+r) >> 1;
+        if(vtr[mid] >= k) r = mid;
+        else l = mid+1;
+    }
+    if(vtr[r] == k) cout << r;
+    else cout << -1;
+}
+
+void binary_search_right(vector<int> &vtr, int k){
+    int l = 0, r = n-1;
+    int mid = 0;
+    while(l < r){
+        mid = (l+r+1) >> 1;
+        if(vtr[mid] <= k) l = mid;
+        else r = mid-1;
+    }
+    if(vtr[r] == k) cout << r;
+    else cout << -1;
+}
+
 void solve(){
-    
+    cin >> n >> q;
+    for(int i = 0;i < n;i++){
+        cin >> vtr[i];
+    }
+    for(int i = 1;i <= q;i++){
+        int k = 0;
+        cin >> k;
+        binary_search_left(vtr, k);
+        cout << " ";
+        binary_search_right(vtr, k);
+        cout << endl;
+    }
 }
 
 int main(){
